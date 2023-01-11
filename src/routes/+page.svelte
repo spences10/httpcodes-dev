2e5ff6f77a9875167a478bd912283d9efe97cdd0
spences10 <spences10apps@gmail.com>
2023-01-11 19:34:07 +0000

    feat: :poop: basic filter for codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { codes } from '$lib/utils'
+
+	let search = ''
+	$: filteredCodes = codes.filter(item =>
+		item.code.toLowerCase().includes(search.toLowerCase()) ||
+    item.message.toLowerCase().includes(search.toLowerCase()) ||
+    item.detail.toLowerCase().includes(search.toLowerCase()) ||
+    item.class.toLowerCase().includes(search.toLowerCase()) 
+	)
+</script>
+
+<input type="text" bind:value={search} />
+{#each filteredCodes as code}
+	<pre>{JSON.stringify(code, null, 2)}</pre>
+{/each}
